52e50e452dcb1e4bb256dba094b48c9ff21b3442
juliozaffari <116125718+juliozaffari@users.noreply.github.com>
2023-08-03 21:57:26 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+var randomNo = Math.floor((Math.random()*array1.length)+1);
+Element_of_array = array1[randomNo];
+console.log(randomNumber);
+
+var timerCounter = 0;
+var timerCheck = "";
+var drawSketch = "";
+var answerHolder= "";
+var score = 0;
